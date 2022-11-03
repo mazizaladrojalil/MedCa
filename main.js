2add9335b482ec463ed2fe6326a7967a2d4efc59
MuhmmdAziz <73269572+mazizaladrojalil@users.noreply.github.com>
2022-11-03 13:52:05 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function openReminder(){
+    document.getElementById("OverlayReminder").style.display = "block";
+}
+
+function closeReminder(){
+    document.getElementById("OverlayReminder").style.display = "none";
+}
+
+function openChatDoc(){
+    document.getElementById("OverlayChatDoc").style.display = "block";
+}
+
+function closeChatDoc(){
+    document.getElementById("OverlayChatDoc").style.display = "none";
+}
